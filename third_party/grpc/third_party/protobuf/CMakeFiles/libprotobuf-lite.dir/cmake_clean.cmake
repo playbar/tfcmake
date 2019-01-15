c2b894145cc612e08dfe3a933eabd9c8722c2fdf
hougl <hgl868@126.com>
2019-01-15 14:33:49 +0800

    添加代码

---
+++ third_party/grpc/third_party/protobuf/CMakeFiles/libprotobuf-lite.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/arena.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/arenastring.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/extension_set.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/generated_message_table_driven_lite.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/generated_message_util.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/io/coded_stream.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/io/zero_copy_stream.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/io/zero_copy_stream_impl_lite.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/message_lite.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/repeated_field.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/atomicops_internals_x86_gcc.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/atomicops_internals_x86_msvc.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/bytestream.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/common.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/int128.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/io_win32.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/once.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/status.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/statusor.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/stringpiece.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/stringprintf.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/structurally_valid.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/strutil.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/stubs/time.cc.o"
+  "CMakeFiles/libprotobuf-lite.dir/src/google/protobuf/wire_format_lite.cc.o"
+  "libprotobuf-lite.pdb"
+  "libprotobuf-lite.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libprotobuf-lite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
