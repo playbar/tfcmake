0a859535b478ce0c7965f847a8beb099df80e13c
hougl <hgl868@126.com>
2019-02-12 15:13:36 +0800

    修改代码

--- example/predictor/simple_model.cc
+++ example/predictor/simple_model.cc
@@ -12,76 +12,79 @@ using namespace tensorflow;
  * @return [description]
  */
 int main(int argc, char* argv[]) {
-  // Initialize a tensorflow session
-  Session* session;
-  Status status = NewSession(SessionOptions(), &session);
-  if (!status.ok()) {
-    std::cerr << status.ToString() << std::endl;
-    return 1;
-  } else {
-    std::cout << "Session created successfully" << std::endl;
-  }
+    // Initialize a tensorflow session
+    Session* session;
+    Status status = NewSession(SessionOptions(), &session);
+    if (!status.ok()) {
+        std::cerr << status.ToString() << std::endl;
+        return 1;
+    } else {
+        std::cout << "Session created successfully" << std::endl;
+    }
 
-  // Load the protobuf graph
-  GraphDef graph_def;
-  std::string graph_path = argv[1];
-  status = ReadBinaryProto(Env::Default(), graph_path, &graph_def);
-  if (!status.ok()) {
-    std::cerr << status.ToString() << std::endl;
-    return 1;
-  } else {
-    std::cout << "Load graph protobuf successfully" << std::endl;
-  }
+    // Load the protobuf graph
+    GraphDef graph_def;
+    std::string graph_path = "graph.pb"; //argv[1];
+    status = ReadBinaryProto(Env::Default(), graph_path, &graph_def);
+    if (!status.ok()) {
+        std::cerr << status.ToString() << std::endl;
+        return 1;
+    } else {
+        std::cout << "Load graph protobuf successfully" << std::endl;
+    }
 
-  // Add the graph to the session
-  status = session->Create(graph_def);
-  if (!status.ok()) {
-    std::cerr << status.ToString() << std::endl;
-    return 1;
-  } else {
-    std::cout << "Add graph to session successfully" << std::endl;
-  }
+    // Add the graph to the session
+    status = session->Create(graph_def);
+    if (!status.ok()) {
+        std::cerr << status.ToString() << std::endl;
+        return 1;
+    } else {
+        std::cout << "Add graph to session successfully" << std::endl;
+    }
 
-  // Setup inputs and outputs:
+    // Setup inputs and outputs:
 
-  // Our graph doesn't require any inputs, since it specifies default values,
-  // but we'll change an input to demonstrate.
-  Tensor a(DT_FLOAT, TensorShape());
-  a.scalar<float>()() = 3.0;
+    // Our graph doesn't require any inputs, since it specifies default values,
+    // but we'll change an input to demonstrate.
+    Tensor a(DT_FLOAT, TensorShape());
+    a.scalar<float>()() = 30.0;
 
-  Tensor b(DT_FLOAT, TensorShape());
-  b.scalar<float>()() = 2.0;
+    Tensor b(DT_FLOAT, TensorShape());
+    b.scalar<float>()() = 2.0;
 
-  std::vector<std::pair<string, tensorflow::Tensor>> inputs = {
-    { "a", a },
-    { "b", b },
-  };
+    std::vector<std::pair<string, tensorflow::Tensor>> inputs = {
+            { "a", a },
+            { "b", b },
+    };
 
-  // The session will initialize the outputs
-  std::vector<tensorflow::Tensor> outputs;
+    // The session will initialize the outputs
+    std::vector<tensorflow::Tensor> outputs;
 
-  // Run the session, evaluating our "c" operation from the graph
-  status = session->Run(inputs, {"c"}, {}, &outputs);
-  if (!status.ok()) {
-    std::cerr << status.ToString() << std::endl;
-    return 1;
-  } else {
-    std::cout << "Run session successfully" << std::endl;
-  }
+    // Run the session, evaluating our "c" operation from the graph
+    status = session->Run(inputs, { "c", "d"}, {}, &outputs);
+    if (!status.ok()) {
+        std::cerr << status.ToString() << std::endl;
+        return 1;
+    } else {
+        std::cout << "Run session successfully" << std::endl;
+    }
 
-  // Grab the first output (we only evaluated one graph node: "c")
-  // and convert the node to a scalar representation.
-  auto output_c = outputs[0].scalar<float>();
+    // Grab the first output (we only evaluated one graph node: "c")
+    // and convert the node to a scalar representation.
+    auto output_c = outputs[0].scalar<float>();
+    auto output_d = outputs[1].scalar<float>();
 
-  // (There are similar methods for vectors and matrices here:
-  // https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/public/tensor.h)
+    // (There are similar methods for vectors and matrices here:
+    // https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/public/tensor.h)
 
-  // Print the results
-  std::cout << outputs[0].DebugString() << std::endl; // Tensor<type: float shape: [] values: 30>
-  std::cout << "output value: " << output_c() << std::endl; // 30
+    // Print the results
+    std::cout << outputs[0].DebugString() << std::endl; // Tensor<type: float shape: [] values: 30>
+    std::cout << "output value: " << output_c() << std::endl; // 30
+    std::cout << outputs[1].DebugString() << std::endl;
+    std::cout << "output value: " << output_d() << std::endl;
 
-  // Free any resources used by the session
-  session->Close();
+    // Free any resources used by the session
+    session->Close();
 
-  return 0;
+    return 0;
 }
--- example/predictor/simple_model/simple_model.py
+++ example/predictor/simple_model/simple_model.py
@@ -7,11 +7,13 @@ with tf.Session() as sess:
     a = tf.Variable(5.0, name='a')
     b = tf.Variable(6.0, name='b')
     c = tf.multiply(a, b, name='c')
+    d = tf.multiply(b, c, name='d')
 
     sess.run(tf.global_variables_initializer())
 
     print(a.eval()) # 5.0
     print(b.eval()) # 6.0
     print(c.eval()) # 30.0
+    print(d.eval())
 
     tf.train.write_graph(sess.graph_def, '../', 'graph.pb', as_text=False)
